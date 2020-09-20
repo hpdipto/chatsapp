5d839145773f0aff459454c9ed86ef8b701f3d94
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-20 21:15:01 +0600

    Formik added in registration form, need some more validation

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,45 +1,138 @@
 import * as React from "react";
 import DatePicker from "react-datepicker";
+import { useFormik } from "formik";
 
 const Register: React.FC = () => {
+	const [errorMessages, setErrorMessages] = React.useState([]);
+
+	const validate = (values: any) => {
+		if (!values.firstName) {
+			setErrorMessages((em) => [...em, "Please enter First Name"]);
+		}
+		if (!values.lastName) {
+			setErrorMessages((em) => [...em, "Please enter Last Name"]);
+		}
+		if (!values.username) {
+			setErrorMessages((em) => [...em, "Please enter Username"]);
+		}
+		if (!values.email) {
+			setErrorMessages((em) => [...em, "Please enter Email"]);
+		}
+		if (!values.password) {
+			setErrorMessages((em) => [...em, "Please enter Password"]);
+		}
+		if (!values.password2) {
+			setErrorMessages((em) => [...em, "Please enter Password again"]);
+		}
+	};
+
+	const formik = useFormik({
+		initialValues: {
+			firstName: "",
+			lastName: "",
+			username: "",
+			email: "",
+			password: "",
+			password2: "",
+			gender: "",
+			dob: "",
+		},
+		validate,
+		validateOnChange: false,
+		validateOnBlur: false,
+		onSubmit: (values) => {
+			let newUser = {
+				...values,
+			};
+
+			if (errorMessages.length > 0) {
+				console.log(errorMessages);
+				setErrorMessages((em) => []);
+			}
+			console.log(newUser);
+		},
+	});
+
 	return (
 		<div className="container mt-3 col-sm-8">
-			<form>
+			<form onSubmit={formik.handleSubmit}>
 				<div className="form-group row">
 					<label htmlFor="firstName" className="col-sm-3">
 						First Name
 					</label>
-					<input type="text" className="form-control col-sm-9 mb-3" />
+					<input
+						type="text"
+						className="form-control col-sm-9 mb-3"
+						id="firstName"
+						value={formik.values.firstName}
+						onChange={formik.handleChange}
+					/>
 
 					<label htmlFor="lastName" className="col-sm-3">
 						Last Name
 					</label>
-					<input type="text" className="form-control col-sm-9 mb-3" />
+					<input
+						type="text"
+						className="form-control col-sm-9 mb-3"
+						id="lastName"
+						value={formik.values.lastName}
+						onChange={formik.handleChange}
+					/>
 
 					<label htmlFor="username" className="col-sm-3">
 						Username
 					</label>
-					<input type="text" className="form-control col-sm-9 mb-3" />
+					<input
+						type="text"
+						className="form-control col-sm-9 mb-3"
+						id="username"
+						value={formik.values.username}
+						onChange={formik.handleChange}
+					/>
 
 					<label htmlFor="email" className="col-sm-3">
 						Email
 					</label>
-					<input type="email" className="form-control col-sm-9 mb-3" />
+					<input
+						type="email"
+						className="form-control col-sm-9 mb-3"
+						id="email"
+						value={formik.values.email}
+						onChange={formik.handleChange}
+					/>
 
 					<label htmlFor="password" className="col-sm-3">
 						Password
 					</label>
-					<input type="password" className="form-control col-sm-9 mb-3" />
+					<input
+						type="password"
+						className="form-control col-sm-9 mb-3"
+						id="password"
+						value={formik.values.password}
+						onChange={formik.handleChange}
+					/>
 
 					<label htmlFor="password2" className="col-sm-3">
 						Password Again
 					</label>
-					<input type="password" className="form-control col-sm-9 mb-3" />
+					<input
+						type="password"
+						className="form-control col-sm-9 mb-3"
+						id="password2"
+						value={formik.values.password2}
+						onChange={formik.handleChange}
+					/>
 
 					<label htmlFor="gender" className="col-sm-3">
 						Gender
 					</label>
-					<select name="gender" className="form-control col-sm-9 mb-3">
+					<select
+						name="gender"
+						className="form-control col-sm-9 mb-3"
+						id="gender"
+						value={formik.values.gender}
+						onChange={formik.handleChange}
+					>
 						<option value="">Choose</option>
 						<option value="female">Female</option>
 						<option value="male">Male</option>
@@ -50,7 +143,13 @@ const Register: React.FC = () => {
 						Date of Birth
 					</label>
 					<div className="col-sm-9 mb-3">
-						<DatePicker className="form-control col-sm" />
+						<DatePicker
+							className="form-control col-sm"
+							id="dob"
+							value={formik.values.dob}
+							selected={formik.values.dob}
+							onChange={(date) => formik.setFieldValue("dob", date)}
+						/>
 					</div>
 
 					<label htmlFor="empty" className="col-sm-3"></label>
