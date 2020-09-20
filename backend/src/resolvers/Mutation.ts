9429b0a6739fd188a9b501b6b6be4d0334e69dcf
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-20 09:05:35 +0600

    Password encryption added

--- backend/src/resolvers/Mutation.ts
+++ backend/src/resolvers/Mutation.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import { User } from "../models/User";
 import { UserType } from "../TYPES";
 
@@ -5,6 +6,8 @@ import { UserType } from "../TYPES";
 const registerUser = (parent: any, args: { user: UserType }) => {
 	let newUser = new User({
 		...args.user,
+		password: bcrypt.hashSync(args.user.password, 11),
+		password2: bcrypt.hashSync(args.user.password2, 11),
 	});
 
 	return newUser.save();
