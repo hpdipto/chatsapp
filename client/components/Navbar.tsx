7cda7b83c19ad68abf616e8591b3dd6593f561e5
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-26 08:18:38 +0600

    Login button added

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -23,7 +23,45 @@ const Navbar: React.FC<{ user: any }> = ({ user }: { user: any }) => {
 				>
 					ChatsApp
 				</h2>
+
+				<div className="dropdown">
+					<img
+						src="/guest.png"
+						alt="user"
+						width="40"
+						style={{ cursor: "pointer" }}
+						className="dropdown-toggle"
+						id="dropdownMenuButton"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false"
+					/>
+					<div
+						className="dropdown-menu dropdown-menu-right"
+						aria-labelledby="dropdownMenuButton"
+					>
+						<a href="/login" className="dropdown-item">
+							Login
+						</a>
+					</div>
+				</div>
 			</nav>
+
+			<script
+				src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+				integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+				crossOrigin="anonymous"
+			></script>
+			<script
+				src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+				integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+				crossOrigin="anonymous"
+			></script>
+			<script
+				src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+				integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+				crossOrigin="anonymous"
+			></script>
 		</div>
 	);
 };
