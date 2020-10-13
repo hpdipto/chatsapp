d8a1168f3e36522ff2de57d39e49b89d9c773f72
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-10-13 12:20:55 +0600

    Finalize project but opens for further development

--- backend/dist/resolvers/Subscription.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const newChatSubscription = {
-    subscribe: (parent, args, context) => context.pubsub.asyncIterator(args.rooms),
-};
-const Subscription = {
-    newChatSubscription,
-};
-exports.default = Subscription;
--- backend/src/TYPES.ts
+++ backend/src/TYPES.ts
@@ -1,40 +1,40 @@
-export type UserType {
-	firstName: string,
-	lastName: string,
-	email: string,
-	username: string,
-	password: string,
-	password2: string,
-	gender?: string,
-	dateOfBirth?: Date,
-	activityStatus?: boolean,
-	chatRooms?: Array<string>,
+export type UserType = {
+	firstName: string;
+	lastName: string;
+	email: string;
+	username: string;
+	password: string;
+	password2: string;
+	gender?: string;
+	dateOfBirth?: Date;
+	activityStatus?: boolean;
+	chatRooms?: Array<string>;
 };
 
-export type Credentials {
-	emailOrUserName: string,
-	password: string
+export type Credentials = {
+	emailOrUserName: string;
+	password: string;
 };
 
-export type RoomType {
-	roomName: string,
-	roomId: string,
-	users: Array<string>,
-	admins: Array<string>,
-	blockedUser?: Array<string>
+export type RoomType = {
+	roomName: string;
+	roomId: string;
+	users: Array<string>;
+	admins: Array<string>;
+	blockedUser?: Array<string>;
 };
 
-export type JoinRoomCredentials {
-	userId: string,
-	queryKey: string,
-	roomId: string
+export type JoinRoomCredentials = {
+	userId: string;
+	queryKey: string;
+	roomId: string;
 };
 
-export type TextBodyType {
-	roomId: string,
-	userName: string,
-	userId: string,
-	queryKey: string,
-	text: string,
-	time: Date
-}
+export type TextBodyType = {
+	roomId: string;
+	userName: string;
+	userId: string;
+	queryKey: string;
+	text: string;
+	time: Date;
+};
